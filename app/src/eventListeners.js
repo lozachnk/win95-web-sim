34dbb11c7d04181c36e71d4ec01b7f14c01f9c87
Ken Lozano <150810410+lozachnk@users.noreply.github.com>
2026-03-20 23:08:41 +0800

    Event listener improvements

--- app/src/eventListeners.js
+++ app/src/eventListeners.js
@@ -6,34 +6,43 @@ function attachWindowEVT() {
   let draggedWindow = null;
   let offset = { x: 0, y: 0 };
 
-  document.addEventListener('mousedown', (e) => {
-    if (e.target.classList.contains('title-bar')) {
-      const win = e.target.closest('.window');
-      const rect = win.getBoundingClientRect();
+  function onMouseDown(e) {
+    const titleBar = e.target.closest('.title-bar');
+    if (!titleBar) return;
 
-      win.style.left = `${rect.left}px`;
-      win.style.top = `${rect.top}px`;
-      win.style.transform = `none`;
+    const win = titleBar.closest('.window');
+    if (!window) return;
 
-      offset = {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-      }
+    const rect = win.getBoundingClientRect();
+    win.style.left = `${rect.left}px`;
+    win.style.top = `${rect.top}px`;
+    win.style.transform = `none`;
 
-      draggedWindow = win;
+    offset = {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
     }
-  });
 
-  document.addEventListener('mousemove', (e) => {
+    draggedWindow = win;
+    document.body.style.userSelect = 'none';
+  }
+  
+  function onMouseMove(e) {
     if (!draggedWindow) return;
 
     draggedWindow.style.top = `${e.clientY - offset.y}px`;
     draggedWindow.style.left = `${e.clientX - offset.x}px`;
-  });
+  }
 
-  document.addEventListener('mouseup', () => {
+  function onMouseUp() {
     if (!draggedWindow) return;
     draggedWindow = null;
-  })
+
+    document.body.style.userSelect = '';
+  }
+
+  document.addEventListener('mousedown', onMouseDown);
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 }
 
